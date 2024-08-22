193d85b2f55de69dc195cc99871c3fbdc9cadaec
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-08-21 19:22:42 -0700

    Add modules, file path/file name, prompt and file writer

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const outputPath = './output';
+const fileNameAndPath = `${outputPath}/README.md`;
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -6,47 +11,76 @@ const questions = [
     name: 'title',
     message: 'What is the title of this project?'
   },
-  {
-    name: 'description',
-    message: 'Please enter a project description'
-  },
-  {
-    name: 'installation',
-    message: 'What are the installation instructions?'
-  },
-  {
-    name: 'usage_info',
-    message: 'Please enter usage information'
-  },
-  {
-    name: 'contribution_guidelines',
-    message: 'What are this project\'s contribution guidelines?'
-  },
-  {
-    name: 'test_instructions',
-    message: 'What are these project\'s test instructions?'
-  },
-  {
-    name: 'license',
-    message: 'What license will this project use?',
-    type: 'list',
-    choices: [
-      'Option A',
-      'Option B'
-    ]
-  },
-  {
-    name: 'username',
-    message: 'What is your github username?'
-  },
-  {
-    name: 'email',
-    message: 'What is your email address?'
-  }
+  // {
+  //   name: 'description',
+  //   message: 'Please enter a project description'
+  // },
+  // {
+  //   name: 'installation',
+  //   message: 'What are the installation instructions?'
+  // },
+  // {
+  //   name: 'usage_info',
+  //   message: 'Please enter usage information'
+  // },
+  // {
+  //   name: 'contribution_guidelines',
+  //   message: 'What are this project\'s contribution guidelines?'
+  // },
+  // {
+  //   name: 'test_instructions',
+  //   message: 'What are these project\'s test instructions?'
+  // },
+  // {
+  //   name: 'license',
+  //   message: 'What license will this project use?',
+  //   type: 'list',
+  //   choices: [
+  //     'Option A',
+  //     'Option B'
+  //   ]
+  // },
+  // {
+  //   name: 'username',
+  //   message: 'What is your github username?'
+  // },
+  // {
+  //   name: 'email',
+  //   message: 'What is your email address?'
+  // }
 ];
 
+// title
+// description, installation instructions, usage information, contribution guidelines, and test instructions
+// license
+// Github username
+// email address
+
+inquirer.prompt(
+  questions
+)
+.then(answers => {
+  writeToFile(answers)
+})
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  fs.stat(outputPath, (error, stats) => {
+    // Create directory if not present
+    if(error) {
+      console.log(error);
+      fs.mkdirSync(outputPath);
+    }
+
+    // Create/Overwrite file with content from answers
+    fs.writeFile(fileNameAndPath, JSON.stringify(data), function(error) {
+        console.log(error);
+    });
+  });
+}
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
